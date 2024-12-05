ec0a8ace3240fa4e89c27f31a8885c769177de42
Wouter Pieter Stekelenburg <woupiestek@yahoo.com>
2024-12-05 23:31:37 +0100

    data orient some more

--- src/nwc-to-musicxml/test_script.ts
+++ src/nwc-to-musicxml/test_script.ts
@@ -1,10 +1,16 @@
-import { Parser } from "./parser.ts";
-import { Scanner } from "./scanner.ts";
-import { transform } from "./transformer.ts";
+// import { Parser } from "./parser.ts";
+// import { Scanner } from "./scanner.ts";
+import { Transformed } from "./transformer.ts";
 
 const source = await Deno.readTextFile("samples\\2008-8-24!.nwctxt");
 try {
-  transform(source);
+  const transformed = new Transformed(source);
+  const dur = new Set(
+    transformed.data.flatMap(({ dur }) =>
+      dur ? (dur instanceof Array ? dur : [dur]) : []
+    ),
+  );
+  console.log(dur);
 } catch (e) {
   console.error("Something went wrong", e);
 }
--- src/nwc-to-musicxml/transformer.ts
+++ src/nwc-to-musicxml/transformer.ts
@@ -1,110 +1,157 @@
 import { Parser } from "./parser.ts";
 import { Scanner } from "./scanner.ts";
 
-type Measure = { number: number; data: any[] };
-type Part = { id: string; name: string; measures: Measure[] };
+export class Transformed {
+  partOffset: number[] = [];
+  partNames: string[] = [];
+  measures: number[] = [];
+  types: string[] = [];
+  data: object[] = [];
 
-export function transform(source: string) {
-  const scanner = new Scanner(source);
-  const nwc = new Parser(scanner).result();
-  let part: Part = { id: "", name: "", measures: [] };
-  let measure: Measure = { number: 1, data: [] };
-  const parts = [];
-  for (const line of nwc.middle) {
-    switch (scanner.getName(line.tag.from + 1)) {
-      // case "SongInfo":
-      case "AddStaff": {
-        let name = "";
-        for (const column of line.columns) {
-          switch (scanner.getName(column.tag.from + 1)) {
-            case "Name":
-              name = scanner.getString(column.values[0].from + 2);
+  #offset = 34;
+  #signature = [0, 0, 0, 0, 0, 0, 0];
+  #alterations = [0, 0, 0, 0, 0, 0, 0];
+
+  static #clefStep: { [_: string]: number } = {
+    Bass: 22,
+    Treble: 34,
+    Alto: 28,
+    Tenor: 26,
+  };
+
+  static #alters: { [_: string]: number } = {
+    x: 2,
+    "#": 1,
+    n: 0,
+    b: -1,
+    v: -2,
+  };
+
+  #pitch(pos: string) {
+    let alter = Transformed.#alters[pos[0]];
+    const offset = this.#offset +
+      +(alter === undefined ? pos : pos.substring(1));
+    if (alter === undefined) {
+      alter = this.#alterations[offset % 7];
+    } else {
+      this.#alterations[offset % 7] = alter;
+    }
+    return { octave: (offset / 7) | 0, step: "CDEFGAB"[offset % 7], alter };
+  }
+
+  constructor(source: string) {
+    const scanner = new Scanner(source);
+    const nwc = new Parser(scanner).result();
+
+    for (const line of nwc.middle) {
+      const typ = scanner.getName(line.tag.from + 1);
+      switch (typ) {
+        // case "SongInfo":
+        case "AddStaff": {
+          this.#signature = [0, 0, 0, 0, 0, 0, 0];
+          this.#alterations = [0, 0, 0, 0, 0, 0, 0];
+          let name = "";
+          for (const column of line.columns) {
+            switch (scanner.getName(column.tag.from + 1)) {
+              case "Name":
+                name = scanner.getString(column.values[0].from + 2);
+            }
           }
+          this.partNames.push(name);
+          this.partOffset.push(this.measures.length);
+          continue;
         }
-        part.measures.push(measure);
-        parts.push(part);
-        part = { id: `P${parts.length}`, name, measures: [] };
-        measure = { number: 1, data: [] };
-        continue;
-      }
-      // case "StaffProperties":
-      // case "StaffInstrument":
-      case "Clef": {
-        const sign = scanner.getName(line.columns[0].values[0].from + 1);
-        const shift = line.columns[1] &&
-          scanner.getName(line.columns[1].values[0].from + 1);
-        measure.data.push({ type: "clef", sign, shift });
-        continue;
-      }
-      case "Key": {
-        const signature = line.columns[0].values.map((it) =>
-          scanner.getKeyPart(it.from + 1)
-        );
-        const tonic = line.columns[1] &&
-          scanner.getKeyPart(line.columns[1].values[0].from + 1);
-        measure.data.push({ type: "key", signature, tonic });
-        continue;
-      }
-      // case "TimeSig":
-      // case "Tempo":
-      // case "Dynamic":
-      case "Rest": {
-        let dur: string[] = [];
-        for (const column of line.columns) {
-          switch (scanner.getName(column.tag.from + 1)) {
-            case "Dur": // todo: triplets
-              dur = column.values.map((it) => scanner.getName(it.from + 1));
+        // case "StaffProperties":
+        // case "StaffInstrument":
+        case "Clef": {
+          const sign = scanner.getName(line.columns[0].values[0].from + 1);
+          const shift = line.columns[1] &&
+            scanner.getName(line.columns[1].values[0].from + 1);
+          this.types.push(typ);
+          this.#offset = Transformed.#clefStep[sign] ?? 34;
+          this.data.push({ sign, shift });
+          continue;
+        }
+        case "Key": {
+          const signature = line.columns[0].values.map((it) =>
+            scanner.getKeyPart(it.from + 1)
+          );
+
+          this.#signature = [0, 0, 0, 0, 0, 0, 0];
+          for (const x of signature) {
+            this.#signature["CDEFGAB".indexOf(x[0])] =
+              { "#": 1, "b": -1 }[x[1]] ?? 0;
           }
+          this.#alterations = [...this.#signature];
+
+          const tonic = line.columns[1] &&
+            scanner.getKeyPart(line.columns[1].values[0].from + 1);
+          this.types.push(typ);
+          this.data.push({ signature, tonic });
+          continue;
         }
-        measure.data.push({ type: "rest", dur });
-        break;
-      }
-      case "Chord": {
-        let dur: string[] = [];
-        let pos: string[] = [];
-        for (const column of line.columns) {
-          switch (scanner.getName(column.tag.from + 1)) {
-            case "Dur": // todo: triplets
-              dur = column.values.map((it) => scanner.getName(it.from + 1));
-              break;
-            case "Pos":
-              pos = column.values.map((it) => scanner.getPos(it.from + 1));
-              break;
+        // case "TimeSig":
+        // case "Tempo":
+        // case "Dynamic":
+        case "Rest": {
+          let dur: string[] = [];
+          for (const column of line.columns) {
+            switch (scanner.getName(column.tag.from + 1)) {
+              case "Dur":
+                dur = column.values.map((it) => scanner.getName(it.from + 1));
+            }
           }
+          this.types.push(typ);
+          this.data.push({ dur });
+          break;
         }
-        measure.data.push({ type: "chord", dur, pos });
-        break;
-      }
-      case "Bar": {
-        part.measures.push(measure);
-        measure = { number: part.measures.length + 1, data: [] };
-        continue;
-      }
-      case "Note": {
-        let dur: string[] = [];
-        let pos = "";
-        for (const column of line.columns) {
-          switch (scanner.getName(column.tag.from + 1)) {
-            case "Dur": // todo: triplets
-              dur = column.values.map((it) => scanner.getName(it.from + 1));
-              break;
-            case "Pos":
-              pos = scanner.getPos(column.values[0].from + 1);
-              break;
+        case "Chord": {
+          let dur: string[] = [];
+          let pos: string[] = [];
+          for (const column of line.columns) {
+            switch (scanner.getName(column.tag.from + 1)) {
+              case "Dur":
+                dur = column.values.map((it) => scanner.getName(it.from + 1));
+                break;
+              case "Pos":
+                pos = column.values.map((it) => scanner.getPos(it.from + 1));
+                break;
+            }
           }
+          this.types.push(typ);
+          this.data.push({ dur, pos, pitch: pos.map((p) => this.#pitch(p)) });
+          break;
         }
-        measure.data.push({ type: "note", dur, pos });
-        break;
+        case "Bar": {
+          this.measures.push(this.data.length);
+          this.#alterations = [...this.#signature];
+          continue;
+        }
+        case "Note": {
+          let dur: string[] = [];
+          let pos = "";
+          for (const column of line.columns) {
+            switch (scanner.getName(column.tag.from + 1)) {
+              case "Dur":
+                dur = column.values.map((it) => scanner.getName(it.from + 1));
+                break;
+              case "Pos":
+                pos = scanner.getPos(column.values[0].from + 1);
+                break;
+            }
+          }
+          this.types.push(typ);
+          this.data.push({
+            dur,
+            pos,
+            pitch: this.#pitch(pos),
+          });
+          break;
+        }
+        // case "SustainPedal":
+        default:
+          continue;
       }
-      // case "SustainPedal":
-      default:
-        continue;
     }
   }
-  if (part) {
-    part.measures.push(measure);
-    parts.push(part);
-  }
-  parts.shift();
-  console.log(JSON.stringify(parts));
 }
